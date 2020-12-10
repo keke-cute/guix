f540bf5dce1109d7917d46a330ac4a9eddaa77c4
Shike Liu <liushike1997@gmail.com>
2020-12-10 19:42:10 +0800

    add i3 in qemu scm

--- qemu-i3.scm
+++ qemu-i3.scm
@@ -1,56 +1,65 @@
-;; This is an operating system configuration generated
-;; by the graphical installer.
+;; this is my guix configuration in qemu
 
+;; Import guix module.
 (use-modules (gnu))
-(use-service-modules desktop networking ssh xorg)
+(use-service-modules desktop networking ssh sddm xorg)
 
 (operating-system
-  (locale "en_US.utf8")
-  (timezone "Asia/Shanghai")
-  (keyboard-layout (keyboard-layout "us"))
-  (host-name "guix")
-  (users (cons* (user-account
-                  (name "keke")
-                  (comment "Shike Liu")
-                  (group "users")
-                  (home-directory "/home/keke")
-                  (supplementary-groups
-                    '("wheel" "netdev" "audio" "video")))
-                %base-user-accounts))
-  (packages
-    (append
-      (list (specification->package "i3-wm")
-            (specification->package "i3status")
-            (specification->package "dmenu")
-            (specification->package "st")
-            (specification->package "nss-certs"))
-      %base-packages))
+ ;; Base system config.
+ (locale "en_US.utf8")
+ (timezone "Asia/Shanghai")
+ (keyboard-layout (keyboard-layout "us"))
+ (host-name "playground")
+ (users (cons* (user-account
+                (name "keke")
+                (comment "Shike Liu")
+                (group "users")
+                (home-directory "/home/keke")
+                (supplementary-groups
+                 '("wheel" "netdev" "audio" "video")))
+               %base-user-accounts))
+ ;; System app list
+ (packages
+  (append
+   (map specification->package
+	`("nss-certs"
+	  "ranger"
+	  "neofetch"
+	  "rofi"
+	  "btrfs-progs"
+	  "git"
+	  "emacs"
+	  "alacritty"))
+   %base-packages))
+ 
+ ;; Base services
   (services
-    (append
-      (list (service
-              openssh-service-type
-              (openssh-configuration
-                (password-authentication? #f)))
-            (set-xorg-configuration
-              (xorg-configuration
-                (keyboard-layout keyboard-layout))))
-      %desktop-services))
-  (bootloader
-    (bootloader-configuration
-      (bootloader grub-efi-bootloader)
-      (target "/boot/efi")
-      (keyboard-layout keyboard-layout)))
-  (swap-devices
-    (list (uuid "0c503864-869a-42a1-bfab-69e0a5a742e7")))
-  (file-systems
-    (cons* (file-system
-             (mount-point "/boot/efi")
-             (device (uuid "D1AB-AC56" 'fat32))
-             (type "vfat"))
-           (file-system
-             (mount-point "/")
-             (device
-               (uuid "984421b9-11f5-4227-8472-2f57382fa1cc"
-                     'ext4))
-             (type "ext4"))
-           %base-file-systems)))
+  (cons*
+   (service openssh-service-type)
+   (service sddm-service-type)
+   (modify-services
+    ;; Remove GDM.
+    (remove (lambda (service)
+              (eq? (service-kind service) gdm-service-type))
+            %desktop-services))))
+  
+ ;; Bootloader
+ (bootloader
+  (bootloader-configuration
+   (bootloader grub-efi-bootloader)
+   (target "/boot/efi")))
+
+ ;;FileSystem
+ (file-systems
+  (cons* (file-system
+          (mount-point "/boot/efi")
+          (device (uuid "D1AB-AC56" 'fat32))
+          (type "vfat"))
+         (file-system
+          (mount-point "/")
+          (device
+           (uuid "984421b9-11f5-4227-8472-2f57382fa1cc"
+                 'btrfs))
+	  (options "compress-force=zstd")
+          (type "btrfs"))
+         %base-file-systems)))
