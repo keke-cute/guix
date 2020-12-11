76ecb985e15e76884dbf4dd29026da89912be68f
不可视界线 <37539103+keke-cute@users.noreply.github.com>
2020-12-11 21:21:15 +0800

    Update mydwm.scm

--- packages/mydwm.scm
+++ packages/mydwm.scm
@@ -1,1 +1,22 @@
-1
+(define-module (my-dwm)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages suckless)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix utils))
+
+(define-public my-dwm
+  (package
+    (inherit dwm)
+    (name "my-dwm")
+    (version "1.0.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://git.example.com/dwm.git")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "111xx222cilx333nj444d4gf8pficjj40jnmfkiwl7ngznjxwkyw"))))))
